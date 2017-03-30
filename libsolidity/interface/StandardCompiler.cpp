336a1ef8482de486e2815b127a643e09695c1dc9
Alex Beregszaszi <alex@rtfs.hu>
2017-03-30 11:16:28 +0100

    Add target selection helpers to StandardCompiler

--- libsolidity/interface/StandardCompiler.cpp
+++ libsolidity/interface/StandardCompiler.cpp
@@ -131,6 +131,62 @@ StringMap createSourceList(Json::Value const& _input)
 	return sources;
 }
 
+bool isTargetRequired(Json::Value const& _targets, string const& _target)
+{
+	for (auto const& target: _targets)
+		/// @TODO support sub-macthing, e.g "evm" matches "evm.assembly"
+		if (target == "*" || target == _target)
+			return true;
+	return false;
+}
+
+///
+/// @_targets is a JSON object containining a two-level hashmap, where the first level is the filename,
+/// the second level is the contract name and the value is an array of target names to be requested for that contract.
+/// @_file is the current file
+/// @_contract is the current contract
+/// @_target is the current target name
+///
+/// Returns true if the _targets has a match for the requested target in the specific file / contract.
+///
+/// In @_targets the use of '*' as a wildcard is permitted.
+///
+/// @TODO optimise this. Perhaps flatten the structure upfront.
+///
+bool isTargetRequired(Json::Value const& _targets, string const& _file, string const& _contract, string const& _target)
+{
+	if (!_targets.isObject())
+		return false;
+
+	for (auto const& file: { _file, string("*") })
+		if (_targets.isMember(file) && _targets[file].isObject())
+		{
+			if (_contract.empty())
+			{
+				/// Special case for SourceUnit-level targets (such as AST)
+				if (
+					_targets[file].isMember("") &&
+					_targets[file][""].isArray() &&
+					isTargetRequired(_targets[file][""], _target)
+				)
+					return true;
+			}
+			else
+			{
+				/// Regular case for Contract-level targets
+				for (auto const& contract: { _contract, string("*") })
+					if (
+						_targets[file].isMember(contract) &&
+						_targets[file][contract].isArray() &&
+						isTargetRequired(_targets[file][contract], _target)
+					)
+						return true;
+			}
+		}
+
+	return false;
+}
+
 Json::Value formatLinkReferences(std::map<size_t, std::string> const& linkReferences)
 {
 	Json::Value ret(Json::objectValue);
